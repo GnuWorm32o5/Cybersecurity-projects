1b2610777991f208a9f5ab6c6bdfb28b58d08c4e
GnuWorm32o5 <stevanglavaskii@gmail.com>
2026-03-26 16:49:58 +0100

    Firewall rules

--- NetworkPacketSniffer/NetworkPacketSniffer.py
+++ NetworkPacketSniffer/NetworkPacketSniffer.py
@@ -2,3 +2,28 @@ import socket
 import struct
 import textwrap
 
+
+
+
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, addr = conn.recvfrom(65536)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print(f"Destination: {dest_mac}, Source: {src_mac}, Protocol: {eth_proto}")
+
+
+#Unpack the ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack("!6s6sH", data[:14])
+    return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+
+#Return the adress fromated
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    return ":".join(bytes_str).uppper()
+
+
+main()
